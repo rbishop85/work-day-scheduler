d994f91299a1f412ca513737a667f46bb2a31d4f
Robert Bishop <rbishop85@gmail.com>
2022-06-24 21:38:13 -0700

    Setup appending of Time Blocks to container object and set each Time Block to display the time in am or pm and color the textarea based on whether it's past, present or future.  Time for comparison is currently a static assigned variable

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ All css already written in css file, disect it to try and find planned html layo
 <!-- // query current day element and tie to a declared variable -->
 <!-- // query container element and tie to declared variable -->
   // This is where you'll render time blocks
-  // add an id to container item to make easier
+  <!-- // add an id to container item to make easier -->
 
 // Render a block for each hour of the day and append to container
  // for( i = 9; i <= 17; i++>) cycle from 9am to 5pm
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,55 @@
 // Current Day element from html file
 var currentDayEl = $("#currentDay");
 // Time to store current time <-- playing with options, not fully setup
-var currentTime = Date.now();
-
-console.log(currentTime);
-console.log((new Date()).getTime());
+var currentTime = 10;
 // Container Element from html file
 var containerEl = $("#container");
 // Array containing hours available
 var workHoursArray = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 
+var i = 0;
+
+var blockTime = 0;
+
+var tense = "";
+
+function DaySchedulerInit() {
+    console.log("Work Day Scheduler is running!")
+    for( i = 0; i < workHoursArray.length; i++) {
+        printBlockTime();
+        determineTense();
+        insertTimeBlock();
+    }
+}
+
+DaySchedulerInit();
+
+function insertTimeBlock() {
+    containerEl.append(`
+    <div class="row time-block">
+        <div class="hour  col-md-2 d-flex justify-content-center align-items-center">${blockTime}</div>
+        <textarea class="col-11 col-md-9 ${tense}"></textarea>
+        <button data-hour="9" class="saveBtn col-1 d-flex justify-content-center align-items-center"><i class="fas fa-save"></i></button>
+    </div>
+    `)
+}
+
+function printBlockTime() {
+    if(workHoursArray[i] < 12){
+        blockTime = (workHoursArray[i] + "AM");
+    } else if(workHoursArray[i] === 12) {
+        blockTime = "12PM";
+    } else {
+        blockTime = ((workHoursArray[i] - 12) + "PM")
+    }
+}
+
+function determineTense() {
+    if (workHoursArray[i] < currentTime) {
+        tense = "past";
+    } else if (workHoursArray[i] === currentTime) {
+        tense = "present";
+    } else {
+        tense = "future";
+    }
+}
